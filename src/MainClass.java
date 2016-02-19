382df507e9c1887ad0d3640066e7ea5a8354b79e
apchanda <apchanda@asu.edu>
2016-02-19 15:46:12 -0700

    AVL tree

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,7 +6,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// findSecondHighestElement();
 		// printZigzag();
-		getDirectoryPath();
+		// getDirectoryPath();
+		AVLTree avlTree = new AVLTree();
 	}
 
 	private static void findSecondHighestElement() {
